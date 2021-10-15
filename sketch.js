91254b8daf2814f95e6b6b8610a4244f3c35facf
srishti0506 <78142550+srishti0506@users.noreply.github.com>
2021-10-16 00:12:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+let s;
+let scl = 20;
+let food;
+
+
+function setup() {
+
+  createCanvas(600,600);
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+}
+
+function pickLocation() {
+  let cols = floor(width / scl);
+  let rows = floor(height / scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
+}
+
+function draw() {
+  background(0);
+  if (s.eat(food)) {
+    pickLocation();
+  }
+  s.death();
+  s.update();
+  s.show();
+  fill('#fae');
+  rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
+}
